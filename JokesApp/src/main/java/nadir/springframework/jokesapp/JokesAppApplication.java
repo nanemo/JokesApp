ee0e23ca916ece6e022c0d5b86c59ae1db4f366f
Nadir Nadirli <companynadirnadirli@gmail.com>
2022-07-13 13:57:51 +0400

    initial add from Spring İnitilizr. Closes #1

--- JokesApp/src/main/java/nadir/springframework/jokesapp/JokesAppApplication.java
+++ JokesApp/src/main/java/nadir/springframework/jokesapp/JokesAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JokesAppApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(JokesAppApplication.class, args);
     }
